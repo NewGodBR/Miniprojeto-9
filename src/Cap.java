1719db44387e16d647f77b347b765a124dc0d0ff
NewGodBR <newgodbr.mine@gmail.com>
2023-04-25 10:49:04 -0300

    save

--- src/Cap.java
+++ src/Cap.java
@@ -1,36 +1,35 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Cap {
   private String text;
   private String titulo;
 
-  public String getTitulo() {
-    return titulo;
-  }
-
-  public String getText() {
-    return text;
-  }
-
+  
   private ArrayList<Escolha> choices;
-
+  
+  private Personagem chars;
+  private int atentionV;
+  
+  public Cap(
+    String texto, String titulo, ArrayList<Escolha> escolhas, Personagem personagem, int atencao) {
+      this.text = texto;
+      this.titulo = titulo;
+      this.choices = escolhas;
+      this.chars = personagem;
+      this.atentionV = atencao;
+    }
+    
+    public String getTitulo() {
+      return titulo;
+    }
+  
+    public String getText() {
+      return text;
+    }
+    
   public ArrayList<Escolha> getChoices() {
     return choices;
   }
-
-  private Personagem chars;
-  private int atentionV;
-
-  public Cap(
-    String texto, String titulo, ArrayList<Escolha> escolhas, Personagem personagem, int atencao) {
-    this.text = texto;
-    this.titulo = titulo;
-    this.choices = escolhas;
-    this.chars = personagem;
-    this.atentionV = atencao;
-  }
-
   public void setChoices(ArrayList<Escolha> escolhas) {
     this.choices = escolhas;
   }
